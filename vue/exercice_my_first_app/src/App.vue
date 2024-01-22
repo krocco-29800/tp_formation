4b7b1c6e6b9fdbcddb8881ee9da42d27153a8ea1
69VFOMT104-14 <marcls29800@gmail.com>
2024-01-22 10:11:39 +0100

    'importation'

---
+++ vue/exercice_my_first_app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+/*  
+CE COMPOSANT RACINE APP RESTE EN MODE COMPOSITION API
+IMPORTER LES COMPOSANTS ICI 
+*/
+import MyOptionComponent from './components/MyOptionComponent.vue';
+</script>
+
+<template>
+  <h1>je suis la</h1>
+<MyOptionComponent></MyOptionComponent>
+  <!-- Monter les composants ici -->
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ vue/exercice_my_first_app/src/components/MyOptionComponent.vue
@@ -0,0 +1,18 @@
+<!-- Ce composant est en mode OPTION API -->
+
+<script >
+export default {
+    name: "MyOptionComponent",
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<template>
+<section>
+    <h2>Hello</h2>
+    <p>{{msg}}</p>
+    
+</section>
+</template>
